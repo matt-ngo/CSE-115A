a5d4ff069ac64cd6fafc48b21724609d82079b10
wchunl <wleung11@ucsc.edu>
2021-02-05 15:49:35 -0800

    added item dropdown, confirm split into subcomponent

--- Splitr/src/App.js
+++ Splitr/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppRouter from './AppRouter';
 import SharedContext from './SharedContext';
 import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {DEFAULT_ITEMS, DEFAULT_FEES} from './DefaultValues';
 
 // custom css: https://material-ui.com/customization/theming/
 const theme = createMuiTheme({
@@ -22,37 +23,18 @@ const theme = createMuiTheme({
   },
 });
 
-// Temporary data for testing
-const TEST_RECEIPT_ITEMS = [
-  {name: 'Frozen yogurt', price: '6.50',
-    isSelected: false, isValid: true, shared: 1},
-  {name: 'Ice cream sandwich', price: '4.99',
-    isSelected: false, isValid: true, shared: 1},
-  {name: 'Eclair', price: '10.00',
-    isSelected: false, isValid: true, shared: 3},
-  {name: 'Cupcake', price: '2.99',
-    isSelected: false, isValid: true, shared: 1},
-  {name: 'Gingerbread', price: '1.99',
-    isSelected: false, isValid: true, shared: 1},
-
-];
-
-const TEST_FEES = {
-  tax: '0.00',
-  tip: '0.00',
-  misc: '0.00',
-};
-
 /**
  *
  * @return {object} JSX
  */
 function App() {
-  // Global state of receipt items, initialized to test data
-  const [receiptItems, setReceiptItems] = React.useState(TEST_RECEIPT_ITEMS);
+  // Global state of receipt items
+  // NOTE: using fake data for testing, should be empty array in final!
+  const [receiptItems, setReceiptItems] = React.useState(DEFAULT_ITEMS);
 
-  // Global state of fees (taxes, tips, etc) initialized to test data
-  const [fees, setFees] = React.useState(TEST_FEES);
+  // Global state of fees (taxes, tips, etc)
+  // NOTE: using fake data for testing, should be empty array in final!
+  const [fees, setFees] = React.useState(DEFAULT_FEES);
 
   // Declaration here to allow manual input option -> edit state on
   const [isEditing, setIsEditing] = React.useState(false);
