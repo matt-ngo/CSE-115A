e8a0c61e926db6972d36dd39e909710f4b982023
Austin Seyboldt <seyboldt.austin@gmail.com>
2021-02-21 11:28:16 -0800

    moves receipt table css into a new file

--- Splitr/src/confirm-components/ReceiptTable.js
+++ Splitr/src/confirm-components/ReceiptTable.js
@@ -1,6 +1,5 @@
 import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
-import {makeStyles} from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
@@ -20,83 +19,7 @@ import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Checkbox from '@material-ui/core/Checkbox';
 import SharedContext from '../SharedContext';
 import {isValidPrice} from '../Confirm';
-
-const useStyles = makeStyles((theme) => ({
-  recTable: {
-    minWidth: '750',
-  },
-  row: {
-    '& > *': {
-      borderBottom: 'unset',
-    },
-  },
-  rowWrappable: {
-    whiteSpace: 'normal',
-    wordWrap: 'break-word',
-  },
-  noGridLine: {
-    borderBottom: 'none',
-  },
-  tableHeader: {
-    fontWeight: 'bold',
-    [theme.breakpoints.down('xs')]: {
-      padding: 5,
-    },
-  },
-  collapsedRow: {
-    paddingBottom: 0,
-    paddingTop: 0,
-  },
-  priceField: {
-    width: 80,
-  },
-  deleteCol: {
-    width: theme.spacing(6),
-  },
-  itemTextField: {
-    fontSize: 14,
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 12,
-    },
-  },
-  priceTextField: {
-    fontSize: 14,
-    textAlign: 'right',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 12,
-    },
-  },
-  removeButton: {
-    position: 'absolute',
-    marginLeft: theme.spacing(3),
-  },
-  selectBox: {
-    width: '50px',
-    [theme.breakpoints.down('xs')]: {
-      padding: 5,
-    },
-  },
-  innerbox: {
-    display: 'inline',
-  },
-  shareButtons: {
-    marginTop: 5,
-    marginBottom: 10,
-    // display: 'flex',
-    // alignItems: 'center',
-  },
-  button: {
-    // marginLeft: '20%',
-    // marginRight: '2%',
-  },
-  cell: {
-    borderBottom: 'none !important',
-    [theme.breakpoints.down('xs')]: {
-      padding: 5,
-    },
-    width: '50px',
-  },
-}));
+import useStyles from '../styles/ReceiptTableStyles';
 
 const calculatePriceEach = (item) => {
   return round(parseFloat(item.price) / item.shared).toFixed(2);
