3dde87cb7430bf9895e146cba875152a3de48f73
Austin Seyboldt <seyboldt.austin@gmail.com>
2021-03-05 17:28:51 -0800

    tiny fixes

--- Splitr/src/DefaultValues.js
+++ Splitr/src/DefaultValues.js
@@ -1,13 +1,12 @@
-export const DEFAULT_ITEMS = [
-];
+export const DEFAULT_ITEMS = [];
 
 export const DEFAULT_FEES = {
   tax: '0.00',
   taxField: '0.00',
-  taxType: '%',
+  taxType: '$',
   tip: '0.00',
-  tipField: '20.00',
-  tipType: '%',
+  tipField: '0.00',
+  tipType: '$',
   misc: '0.00',
   miscField: '0.00',
   miscType: '$',
